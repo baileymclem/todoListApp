3159a67c67ff40512ed272a391905d1e662d9a2c
Bailey Clemons <baileymclem@gmail.com>
2021-05-27 14:19:12 -0700

    "added controllers, routes, and updated server.js"

--- server.js
+++ server.js
@@ -1,6 +1,19 @@
 var express = require("express"),
     app = express(),
-    port = process.env.PORT || 3000;
+    port = process.env.PORT || 3000,
+    mongoose = require("mongoose"),
+    Task = require("./api/models/todoListModel"); // created model loading here
+    // bodyParser = require("body-parser");
+
+    // mongoose instance connection url
+    mongoose.Promise = global.Promise;
+    mongoose.connect("mongodb://localhost/Tododb");
+
+    app.use(express.urlencoded());
+    app.use(express.json()); // parse JSON bodies
+
+    var routes = require("./api/routes/todoListRoutes") // importing route
+    routes(app); // register the route
 
     app.listen(port);
 
